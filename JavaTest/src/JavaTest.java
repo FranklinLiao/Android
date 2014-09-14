698b013ecba8504855aa4225fc8b96e5f069c057
FranklinLiao <1120205104@qq.com>
2014-09-14 22:54:58 +0800

    第二次修改

--- JavaTest/src/JavaTest.java
+++ JavaTest/src/JavaTest.java
@@ -7,6 +7,7 @@ public class JavaTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello,world!");
+		System.out.println("Second world!");
 	}
 
 }
